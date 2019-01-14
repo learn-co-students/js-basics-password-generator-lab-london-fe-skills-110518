133f0964866bcbffe9aec7023dd16aee3e05cfc4
lauren kernan <lauren.kernan17@gmail.com>
2019-01-14 19:08:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 window.addEventListener("load", () => {
   //start your code here
+    // let length = getElementById("length");
+    
+    
+  function passwordGenerator(len){
+    let charset1 = ["a","b","c","d","e","f","g","h","i","j","k","l","m", "n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    let charset2 = ["A","B","C","D","E","F","G","H","I","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    let charset3 = ["0","1","2","3","4","5","6","7","8","9"];
+    let charset4 = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","/","]","^","_","{","|","}","~"];
+    
+    
+    let allCharacters = [...charset1,...charset2,...charset3,...charset4];
+    console.log(allCharacters);
+
+    let password="";
+    
+    for (var i=0 ; i < len; i++){
+      let length2 = Math.floor(Math.random() * allCharacters.length);
+      
+      password += allCharacters[length2];
+      console.log(length2);
+    }
+    return password;
+  }
+  
+  console.log(passwordGenerator(7));
+  document.getElementById("result").innerHTML = passwordGenerator(length);
 });
